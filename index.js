9e78cbe7c96b27bb8ead0c9b6f0770da3f56619e
VaneSolis <vsolisperaza01@gmail.com>
2024-01-17 12:12:04 -0600

    primer commit de 3era preentrega

---
+++ index.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const opcionesGuardadas = obtenerDelStorage("opciones");
+    const opciones = opcionesGuardadas ? JSON.parse(opcionesGuardadas) : [];
+
+    document.getElementById("botonSimular").addEventListener("click", function () {
+        console.log("Botón clickeado");
+        const opcionInput = document.getElementById("opcionInput");
+        const opcionSeleccionada = parseInt(opcionInput.value);
+        simularCuotas(opcionSeleccionada, opciones);
+        guardarEnStorage("opciones", JSON.stringify(opciones));
+    });
+});
+
+function simularCuotas(opcion, opciones) {
+    const opcionSeleccionada = opciones.find((_, index) => index + 1 === opcion);
+    if (opcionSeleccionada) {
+        const { capital, cuotas } = opcionSeleccionada;
+        const interes = 0.2;
+        const subtotal1 = capital * interes;
+        const subtotal2 = capital + subtotal1;
+        const cuotaMensual = (subtotal2 / cuotas).toFixed(2);
+        actualizarDOM(cuotaMensual);
+    } else {
+        alert("Opción incorrecta");
+    }
+}
+
+function actualizarDOM(resultado) {
+    const elementoResultado = document.getElementById("resultadoCuotas");
+    elementoResultado.innerHTML = `Pagarás ${resultado} mensuales`;
+}
+
+function guardarEnStorage(clave, valor) {
+    localStorage.setItem(clave, valor);
+}
+
+function obtenerDelStorage(clave) {
+    return localStorage.getItem(clave);
+}
